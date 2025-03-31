4d739878a86e28fa3f4122aa8ed7cff2aae71c3f
kirill <kirya.abbasov.02@mail.ru>
2025-03-31 17:04:51 +0300

    add ProjectWorkshopServiceTest

---
+++ student-app-back/src/test/java/org/example/service/AuthServiceTest.java
@@ -0,0 +1,17 @@
+package org.example.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class AuthServiceTest {
+
+    @Test
+    void authenticateUser() {
+
+    }
+
+    @Test
+    void registerUserInKeycloak() {
+        
+    }
+}
